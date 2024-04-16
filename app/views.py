75f235d9d5cbbdf0ce34a2f1fcbb6c339565c128
Musab <musabab@ac.sce.ac.il>
2024-04-16 19:32:25 +0300

    משתמש קיים יכול להוסיף מוצר לדף המוצרים שלו.

--- app/views.py
+++ app/views.py
@@ -69,6 +69,7 @@ def add_product(request):#
     if request.method == 'POST':#
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():#
+
             product = form.save(commit=False)
             product.user = request.user
             product.save()
